e156dcde5ef9052253dc6561aef574b1c374153b
Giedrius <giedrius@blokas.io>
2020-04-06 15:16:18 +0100

    blokas-jack 1.0.1, autostart and enable Jack

--- blokas-jack/build-deb.sh
+++ blokas-jack/build-deb.sh
@@ -4,4 +4,15 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
 
-fakeroot dpkg --build debroot blokas-jack.deb
+PACKAGE=blokas-jack
+VERSION=1.0.1
+
+PKGFOLDER=${PACKAGE}-${VERSION}
+
+mkdir -p ${PKGFOLDER}
+cp -r src/* ${PKGFOLDER}
+tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
+
+cd ${PKGFOLDER}
+
+echo y | debuild -b -us -uc
