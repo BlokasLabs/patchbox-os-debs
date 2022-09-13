e363898d1e2be1bfc28646f9b21069f838fb81f5
Giedrius <giedrius@blokas.io>
2022-09-13 16:49:41 +0300

    python3-unicategories package

---
+++ python3-unicategories/build-deb.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+cd $DIR
+
+PACKAGE=python3-unicategories
+VERSION=0.1.1
+
+PKGFOLDER=${PACKAGE}-${VERSION}
+
+mkdir -p ${PKGFOLDER}
+cp -r src/* ${PKGFOLDER}
+tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
+
+cd ${PKGFOLDER}
+
+echo y | debuild -b -us -uc
