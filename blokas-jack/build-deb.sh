35bf977aacd2a3a6a7078875ae95733c6b0fb534
Giedrius <giedrius@blokas.io>
2024-02-12 14:21:51 +0200

    blokas-jack 1.0.4-1

--- blokas-jack/build-deb.sh
+++ blokas-jack/build-deb.sh
@@ -1,18 +1,2 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cd $DIR
-
-PACKAGE=blokas-jack
-VERSION=1.0.3
-
-PKGFOLDER=${PACKAGE}-${VERSION}
-
-mkdir -p ${PKGFOLDER}
-cp -r src/* ${PKGFOLDER}
-tar -czf ${PKGFOLDER}.tar.gz --exclude .git ${PKGFOLDER}
-
-cd ${PKGFOLDER}
-
-echo y | debuild -b -us -uc
+#!/bin/sh
+dpkg-buildpackage -b
